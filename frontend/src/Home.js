b186a5496462de39dc73f9f9b68fbdd06b95b6a8
Gabriel Alessandro Sosa <gasosa2811@gmail.com>
2021-09-23 21:34:41 -0300

    mejorias estilos y mensaje confirmacion

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -15,21 +15,43 @@ class Home extends Component {
 
     onSubmit(e) {
         e.preventDefault();
-        var title = this.title.value;
+        let title = this.title.value;
         console.log(title);
-        axios.post('http://ec2-54-94-81-90.sa-east-1.compute.amazonaws.com:4000/enviarMail', {texto: title});
-        this.title.value = "";
+        if(this.title.value){
+            //axios.post('http://ec2-54-94-81-90.sa-east-1.compute.amazonaws.com:4000/enviarMail', {texto: title});
+            this.title.value = "";
+            this.setState({sent: true});
+        }
+    }
+
+    returnHome() {
+        this.setState({sent: false});
     }
 
     render() {
+        const { sent } = this.state;
+        const texto = "Su confesion ha sido enviada y se publicara tras ser validada por nuestros escla... muy bien pagados moderadores. Gracias por su paciencia."
+        const firma = "-Con amor niñita"
         return (
             <div className="container">
                 <div className="area-container">
                     <img src={logo} className="logo"/>
-                    <form className="text-area">
-                        <textarea placeholder="Escribe tu confesión aquí..." ref={(c) => this.title = c} name="title"/>
-                        <button type="submit" onClick={(e) => this.onSubmit(e)} className="submit-button"> Enviar</button>
-                    </form>
+                    {!sent ? (
+                        <form className="text-area">
+                            <textarea placeholder="Escribe tu confesión aquí..." ref={(c) => this.title = c} name="title"/>
+                            <button type="submit" onClick={(e) => this.onSubmit(e)} className="submit-button"> Enviar</button>
+                        </form>
+                    ) : (
+                        <div className="sent-text">
+                            <div className="text">
+                                <p>{texto}</p>
+                                <p className="right">{firma}</p>
+                            </div>
+                            <div>
+                                <button type="button" onClick={(e) => this.returnHome(e)} className="return-button">Volver al inicio</button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         )
